cab5229a8fe961b4d20cf1c03729548f0ca2a59c
Steven Kang <steven.kang@procore.com>
2022-08-29 21:48:07 -0700

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by StevenKang on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
